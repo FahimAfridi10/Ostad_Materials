c7cb2bdfe5b0931251892e3f056a621f1aad65cc
FahimAfridi10 <fahimurfahimur@gmail.com>
2025-04-06 23:23:34 +0600

    first commit

---
+++ bin/livetest1.dart
@@ -0,0 +1,30 @@
+void main() {
+  List<Map<String, dynamic>> students = [
+    {
+      "name": "Alice",
+      "scores": [85, 90, 78],
+    },
+    {
+      "name": "Bob",
+      "scores": [88, 76, 95],
+    },
+    {
+      "name": "Charlie",
+      "scores": [90, 92, 85],
+    },
+  ];
+
+  List<MapEntry<String, double>> averages =
+      students.map((student) {
+        String name = student['name'];
+        List<int> scores = List<int>.from(student['scores']);
+        double avg = scores.reduce((a, b) => a + b) / scores.length;
+        return MapEntry(name, double.parse(avg.toStringAsFixed(2)));
+      }).toList();
+
+  averages.sort((a, b) => b.value.compareTo(a.value));
+
+  Map<String, double> sortedMap = Map.fromEntries(averages);
+
+  print(sortedMap);
+}
